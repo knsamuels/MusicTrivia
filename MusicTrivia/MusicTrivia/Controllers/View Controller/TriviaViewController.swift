eba2fc575847b724d26d341dd7e81b72007b6d83
Julia Carlsson <julia.carlsson@live.com>
2020-07-09 01:10:01 +0300

    created initial design on Storyboard. connected IBOutlets to TriviaViewController.

--- MusicTrivia/MusicTrivia/Controllers/View Controller/TriviaViewController.swift
+++ MusicTrivia/MusicTrivia/Controllers/View Controller/TriviaViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class TriviaViewController: UIViewController {
 
+    //MARK: - Properties
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
-
+    @IBAction func trueButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func falseButtonTapped(_ sender: UIButton) {
+    }
+    
 }
