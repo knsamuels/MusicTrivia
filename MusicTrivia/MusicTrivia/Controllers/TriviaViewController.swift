0b122cef35330cf689877e1a10d0ad983039c10d
Bryan Workman <bryan@bryans-mbp.lan>
2020-07-08 13:02:24 -0700

    start viewController file

---
+++ MusicTrivia/MusicTrivia/Controllers/TriviaViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TriviaViewController.swift
+//  MusicTrivia
+//
+//  Created by Bryan Workman on 7/8/20.
+//  Copyright © 2020 Kristin Samuels . All rights reserved.
+//
+
+import UIKit
+
+class TriviaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
