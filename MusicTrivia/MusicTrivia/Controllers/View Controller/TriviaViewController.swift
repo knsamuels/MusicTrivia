c1b743ccd7bf8e0e0068626796d27e477b16e3ac
Bryan Workman <bryan@bryans-mbp.lan>
2020-07-09 09:10:18 -0700

    fetch function and new outlets

--- MusicTrivia/MusicTrivia/Controllers/View Controller/TriviaViewController.swift
+++ MusicTrivia/MusicTrivia/Controllers/View Controller/TriviaViewController.swift
@@ -14,28 +14,59 @@ class TriviaViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet weak var trueButton: UIButton!
+    var correctAnswer: String = ""
+    var score: Int = 0
+    @IBOutlet weak var falseButton: UIButton!
+    
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchQuestionAndUpdateViews()
     }
     
     //MARK: - Actions
     @IBAction func trueButtonTapped(_ sender: UIButton) {
         //if tapped & correct, change to green and add to score
         //wait 0.2 seconds and update screen
+        checkAnswerTrue()
         
     }
     
     @IBAction func falseButtonTapped(_ sender: UIButton) {
-        
+        checkAnswerFalse()
         
     }
     
     
     //MARK: - Helper Methods
+    func checkAnswerTrue() {
+        if correctAnswer == "True" {
+            score += 1
+            
+            
+        }
+        Timer.scheduledTimer(timeInterval: 0.2, target:self, selector: #selector(fetchQuestionAndUpdateViews), userInfo:nil, repeats: false)
+    }
     
-    //fetch question & answer -- set questionLabel
+    func checkAnswerFalse() {
+        
+    }
+    
+    @objc func fetchQuestionAndUpdateViews() {
+        MusicTriviaController.fetchTrivia { (result) in
+            switch result {
+            case .success(let MusicTrivia):
+                DispatchQueue.main.async {
+                    self.questionLabel.text = MusicTrivia.question
+                    self.correctAnswer = MusicTrivia.correctAnswer
+                }
+            case .failure(let error):
+                print("There was an error fetching trivia questions -- \(error) -- \(error.localizedDescription)")
+            }
+        }
+    }
+    
     
     
 }
